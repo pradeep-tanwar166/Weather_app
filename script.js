59b698a8af17b30a2aab4aa8e0ee9e212f171dc6
Pradeep tanwar <pradeeptanwar166@gmail.com>
2025-07-08 11:03:34 +0530

    weather app
    This is amazing weather app used to create with the help of api.

---
+++ script.js
@@ -0,0 +1,17 @@
+const api_key="e4cca017ad9254f1fa6baca1b12f9a40";
+
+const getweather=async()=>{
+    const place=document.querySelector("#place").value;
+const URL=`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${api_key}&units=metric`;
+    let response=await fetch(URL);
+    console.log(response);
+    let data=await response.json();// understandable to human means everyone can read this .
+    console.log(data);
+    console.log(`Temperature in ${data.name}: ${data.main.temp}°C`)
+    
+para.innerText=`Temperature in ${data.name}:${data.wind.speed}: ${data.main.temp}°C`;
+}
+
+document.querySelector("#search-button").addEventListener("click",getweather);
+
+let para=document.querySelector("#para");
